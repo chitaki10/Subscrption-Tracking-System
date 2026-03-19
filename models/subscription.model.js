c232d40f25b7a48a9e6cbe2bb1df508ed3267e96
chitaki10 <chitakin876@gmail.com>
2026-03-19 22:03:48 +0530

    update subscription model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -54,11 +54,58 @@ const subscriptionSchema = new mongoose.Schema(
       enum: ["active", "cancelled", "expired"],
       default: "active",
     },
+
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date must be in the past or today",
+      },
+    },
+
+    renewalDate: {
+      type: Date,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: "Renewal date must be after the start date",
+      },
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+
+subscriptionSchema.pre('save',function(next){
+    if(!this.renewalDate){
+        const renewalPeriods = {
+            daily :1,
+            weekly :7,
+            monthly:30,
+            yearly : 365,
+        };
+        this.renewalDate = new Date(this.startDate);
+this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
+    }
+
+    if(this.renewalDate < new Date()){
+        this.status = 'expired';
+    }
+
+    next();
+});
+
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
